f31c79f8c3cc1056c73986476d0d7c3e9ade6827
Erki <erkiparna7@gmail.com>
2022-12-08 08:54:36 +0200

    Added API data to project template file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+const fetch = require('node-fetch');
+
+const key = '9f6775d0501bfeb651cc6efcaf868354';
+let city = 'Tartu';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+        .then(function(resp) {
+            return resp.json()
+        })
+        .then(function(data) {
+            let description = data.weather[0].description;
+            let city = data.name;
+            let temp = Math.round(parseFloat(data.main.temp)-273.15);
+            console.log(description)
+            console.log(city)
+            console.log(temp)
+            res.render('index', {
+              description: description,
+              city: city,
+              temp: temp
+            });
+        })
+        .catch(function() {
+            // catch any errors
+        })
 });
 
 module.exports = router;
